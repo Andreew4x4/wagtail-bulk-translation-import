1dac85fc13b6a10943896506e95847bc035843de
Paweł Gruszka <pgruszka@opera.com>
2024-06-12 15:49:21 +0200

    WIP: handle multiple po files upload

--- example/translation_bulk_import/views/edit_translation.py
+++ example/translation_bulk_import/views/edit_translation.py
@@ -1,6 +1,27 @@
+import polib
+
+from django.contrib import messages
 from django.views.decorators.http import require_POST
+from django.utils.translation import gettext as _
+
+from wagtail_localize.models import Translation
 
 
 @require_POST
 def bulk_upload_pofile(request):
-    pass
+    files = request.FILES.getlist("file")
+    for f in files:
+        try:
+            po = polib.pofile(f.read())
+            translation_id = po.metadata["X-WagtailLocalize-TranslationID"]
+            translation = Translation.objects.get(uuid=translation_id)
+            translation.import_po(po)
+            messages.success(
+                request,
+                _(f"Successfully imported translations from PO File ({translation})."),
+            )
+        except (OSError, UnicodeDecodeError):
+            # Annoyingly, POLib uses OSError for parser exceptions...
+            messages.error(request, _("Please upload a valid PO file."))
+        except Translation.DoesNotExist:
+            messages.error(request, _("Please upload a valid PO file."))
