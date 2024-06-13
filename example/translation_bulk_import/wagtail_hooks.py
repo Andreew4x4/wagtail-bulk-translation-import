97192127e0abca7dcb95ae4e78fdd36a99134a39
Andrzej Kudas <akudas@opera.com>
2024-06-13 11:22:56 +0200

    Move buttons to menu

--- example/translation_bulk_import/wagtail_hooks.py
+++ example/translation_bulk_import/wagtail_hooks.py
@@ -6,6 +6,7 @@ from urllib.parse import urlencode
 from wagtail import hooks
 from wagtail.models import Locale, Page
 from wagtail.admin.widgets import ListingButton
+from wagtail.admin.menu import MenuItem
 
 from .views import edit_translation
 
@@ -28,6 +29,12 @@ def register_admin_urls():
             edit_translation.BulkDownload.as_view(),
             name="bulk_download_files",
         ),
+
+        path(
+            "translate/download_bulk_files/",
+            edit_translation.BulkDownload.as_view(),
+            name="bulk_download_all_files",
+        ),
     ]
 
     return [
@@ -41,20 +48,30 @@ def register_admin_urls():
     ]
 
 
-def page_listing_upload_po_button(page: Page, user, view_name=None, next_url=None):
+@hooks.register('register_admin_menu_item')
+def menu_item_upload_po_button():
     url = reverse(
         "bulk_translation_import:bulk_upload_files",
     )
-    yield ListingButton(
-        _("Upload many .PO files"),
+    return MenuItem(
+        _("Upload translations"),
         url,
-        priority=90,
+        order=9999,
         icon_name="upload",
     )
 
 
-hooks.register("register_page_header_buttons", page_listing_upload_po_button)
-hooks.register("register_page_listing_more_buttons", page_listing_upload_po_button)
+@hooks.register('register_admin_menu_item')
+def menu_item_download_po_button():
+    url = reverse(
+        "bulk_translation_import:bulk_download_all_files",
+    )
+    return MenuItem(
+        _("Download translations"),
+        url,
+        order=9999,
+        icon_name="download",
+    )
 
 
 def page_listing_download_po_button(page: Page, user, view_name=None, next_url=None):
@@ -66,11 +83,12 @@ def page_listing_download_po_button(page: Page, user, view_name=None, next_url=N
         if next_url is not None:
             url += "?" + urlencode({"next": next_url})
         yield ListingButton(
-            _("Download .PO files for this site"),
+            _("Download .PO files for this page"),
             url,
             priority=91,
             icon_name="download",
         )
+
 
 hooks.register("register_page_header_buttons", page_listing_download_po_button)
 hooks.register("register_page_listing_more_buttons", page_listing_download_po_button)
