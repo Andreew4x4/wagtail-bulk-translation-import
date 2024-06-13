da288bf8064b20c69498ce793afe0d8d4adc124c
Andrzej Kudas <akudas@opera.com>
2024-06-13 14:07:03 +0200

    ui amendmends

--- example/translation_bulk_import/wagtail_hooks.py
+++ example/translation_bulk_import/wagtail_hooks.py
@@ -1,5 +1,7 @@
 from django.urls import include, path, reverse
 from django.utils.translation import gettext as _
+from django.utils.html import format_html
+from django.templatetags.static import static
 
 from urllib.parse import urlencode
 
@@ -7,6 +9,7 @@ from wagtail import hooks
 from wagtail.models import Page
 from wagtail.admin.widgets import ListingButton
 
+from wagtail.admin.menu import Menu, MenuItem, SubmenuMenuItem
 
 from .views import edit_translation
 from .models import TranslationMenuItem
@@ -47,26 +50,24 @@ def register_admin_urls():
     ]
 
 
-@hooks.register('register_admin_menu_item')
 def menu_item_upload_po_button():
     url = reverse(
         "bulk_translation_import:bulk_upload_files",
     )
     return TranslationMenuItem(
-        _("Upload translations"),
+        _("Bulk upload"),
         url,
         order=9999,
         icon_name="upload",
     )
 
 
-@hooks.register('register_admin_menu_item')
 def menu_item_download_po_button():
     url = reverse(
         "bulk_translation_import:bulk_download_all",
     )
     return TranslationMenuItem(
-        _("Download translations"),
+        _("Bulk download"),
         url,
         order=9999,
         icon_name="download",
@@ -91,3 +92,17 @@ def page_listing_download_po_button(page: Page, user, view_name=None, next_url=N
 
 hooks.register("register_page_header_buttons", page_listing_download_po_button)
 hooks.register("register_page_listing_more_buttons", page_listing_download_po_button)
+
+@hooks.register('register_admin_menu_item')
+def register_translation_menu_item():
+    translation_menu = Menu(
+        items=[
+            menu_item_upload_po_button(),
+            menu_item_download_po_button()
+        ]
+    )
+    return SubmenuMenuItem('Translations', translation_menu, icon_name='wagtail-localize-language')
+
+@hooks.register('insert_global_admin_css')
+def global_admin_css():
+    return format_html('<link rel="stylesheet" href="{}">', static('my/wagtail/theme.css'))
